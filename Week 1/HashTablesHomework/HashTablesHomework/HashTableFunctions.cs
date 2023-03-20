0456152c6980a63107d721663fe9aeb33ce66cf7
Vladislav Kotsev <vladygk8@gmaila.com>
2023-03-20 12:19:56 +0200

    Add task2 and task3

--- Week 1/HashTablesHomework/HashTablesHomework/HashTableFunctions.cs
+++ Week 1/HashTablesHomework/HashTablesHomework/HashTableFunctions.cs
@@ -20,5 +20,42 @@
 
             return result;
         }
+
+        public static int[] FindIntersectionOfTwoArrays(int[] arrA, int[] arrB)
+        {
+            var hashSet1 = new HashSet<int>(arrA);
+            var hashSet2 = new HashSet<int>(arrB);
+
+            return hashSet1.Intersect(hashSet2).ToArray();
+        }
+
+        public static List<char> FindUniqueChars(string input, out int indexFirstUnique)
+        {
+            Dictionary<char, int> charDict = new Dictionary<char, int>();
+
+            foreach(char c in input)
+            {
+                if (!charDict.ContainsKey(c))
+                {
+                    charDict.Add(c, 0);
+                }
+                charDict[c]++;
+            }
+
+            List<char> uniqueChars =  charDict.Where(kvp => kvp.Value ==1).Select(kvp=>kvp.Key).ToList();
+
+
+            char firstUnique = uniqueChars.FirstOrDefault();
+            if(firstUnique == null)
+            {
+                indexFirstUnique = -1;
+            }
+            else
+            {
+                indexFirstUnique = input.IndexOf(firstUnique);
+            }
+
+            return uniqueChars;
+        }
     }
 }
--- Week 1/HashTablesHomework/TestHashtableFunctions/TestHashtableHomework.cs
+++ Week 1/HashTablesHomework/TestHashtableFunctions/TestHashtableHomework.cs
@@ -22,5 +22,86 @@ namespace TestHashtableFunctions
             Assert.AreEqual(actualCoolPlateOwner, expectedCoolPlateOwner);
             CollectionAssert.AreEqual(actualOnwersWithMultipleCars, expectedOnwersWithMultipleCars);
         }
+
+        [TestMethod]
+        public void TestFindIntersectionOfTwoArraysWithIntersectingElements()
+        {
+            //Arrange
+            int[] arrA = new int[] { 1, 2, 3, 4, 5 };
+            int[] arrB = new int[] { 1, 2 };
+
+            int[] expected = new int[] { 1, 2 };
+            //Act 
+
+            int[] actual = HashTableFunctions.FindIntersectionOfTwoArrays(arrA, arrB);
+
+            //Assert
+            CollectionAssert.AreEqual(expected,actual);
+        }
+        [TestMethod]
+        public void TestFindIntersectionOfTwoArraysWithoutIntersectingElements()
+        {
+            //Arrange
+            int[] arrA = new int[] { 1, 2, 3, 4, 5 };
+            int[] arrB = new int[] { 6,7 };
+
+            int[] expected = Array.Empty<int>();
+            //Act 
+
+            int[] actual = HashTableFunctions.FindIntersectionOfTwoArrays(arrA, arrB);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFindIntersectionOfTwoEmptyArrays()
+        {
+            //Arrange
+            int[] arrA = Array.Empty<int>();
+            int[] arrB = Array.Empty<int>();
+
+            int[] expected = Array.Empty<int>();
+            //Act 
+
+            int[] actual = HashTableFunctions.FindIntersectionOfTwoArrays(arrA, arrB);
+
+            //Assert
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestFindUniqueCharsGivenUniqueChars()
+        {
+            //Arrange 
+            string input = "aabcvc";
+            int expectedIndex = 2;
+            List<char> exptectedList = new List<char>() { 'b', 'v' };
+
+            //Act
+            int actualIndex = -1;
+            List<char> actualList = HashTableFunctions.FindUniqueChars(input, out actualIndex);
+
+            //Assert
+            Assert.AreEqual(expectedIndex, actualIndex);
+            CollectionAssert.AreEqual(exptectedList, actualList);
+        }
+
+        [TestMethod]
+        public void TestFindUniqueCharsWithoutUniqueChars()
+        {
+            //Arrange 
+            string input = "aabbcc";
+            int expectedIndex = -1;
+            List<char> exptectedList = new List<char>();
+
+            //Act
+            int actualIndex = -1;
+            List<char> actualList = HashTableFunctions.FindUniqueChars(input, out actualIndex);
+
+            //Assert
+            Assert.AreEqual(expectedIndex, actualIndex);
+            CollectionAssert.AreEqual(exptectedList, actualList);
+        }
     }
 }
