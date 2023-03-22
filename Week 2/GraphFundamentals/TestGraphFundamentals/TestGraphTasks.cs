a7303ffbfe7945c750c4480d72ed14038460a4b7
Vladislav Kotsev <vladygk8@gmaila.com>
2023-03-22 12:33:36 +0200

    Add task2

--- Week 2/GraphFundamentals/TestGraphFundamentals/TestGraphTasks.cs
+++ Week 2/GraphFundamentals/TestGraphFundamentals/TestGraphTasks.cs
@@ -11,7 +11,7 @@ public class TestGraphTasks
     {
         //Arrange
         List<List<int>> graphC = new List<List<int>>();
-        graphC.Add(new List<int>() { 1 }); 
+        graphC.Add(new List<int>() { 1 });
         graphC.Add(new List<int>() { 2 });
         graphC.Add(new List<int>() { 3, 0 });
         graphC.Add(new List<int>());
@@ -28,7 +28,7 @@ public class TestGraphTasks
     {
         //Arrange
         List<List<int>> graphC = new List<List<int>>();
-        graphC.Add(new List<int>() { 1 }); 
+        graphC.Add(new List<int>() { 1 });
         graphC.Add(new List<int>() { 2 });
         graphC.Add(new List<int>() { 3 });
         graphC.Add(new List<int>());
@@ -38,5 +38,48 @@ public class TestGraphTasks
 
         //Assert
         Assert.IsFalse(existCycle);
+    }
+
+    [TestMethod]
+    public void TestCalculateFieldsGivenNonCyclicGraph()
+    {
+        //Arrange
+        List<List<int>> graph = new List<List<int>>();
+        graph.Add(new List<int>() { 1, 2 }); // 0 = 1+2
+        graph.Add(new List<int>()); // 1 is numeric
+        graph.Add(new List<int>()); //2 is numeric
+        graph.Add(new List<int>() { 4 }); // 3 = 4
+        graph.Add(new List<int>()); //4 is numeric
+
+        Dictionary<int, int> expectedCalculatedValues = new Dictionary<int, int>()
+        {
+            {0,3 },
+            {1, 1},
+            {2,2 },
+            {3,4 },
+            {4,4 }
+        };
+
+        //Act 
+        Dictionary<int, int> actualCalculatedValues = CalculateValueOfFieldsFunction.CalculateFields(graph);
+
+        //Assert
+        CollectionAssert.AreEqual(expectedCalculatedValues, actualCalculatedValues);
+    }
+
+    [TestMethod]
+    public void TestCalculateFieldsGivenCyclicGraph()
+    {
+        //Arrange
+        List<List<int>> graph = new List<List<int>>();
+        graph.Add(new List<int>() { 1,}); // 0 depends on 1 
+        graph.Add(new List<int> { 0 }); // 1 depends on 0
+    
+
+        //Act + Assert
+        Assert.ThrowsException<ArgumentException>(() =>
+        {
+            CalculateValueOfFieldsFunction.CalculateFields(graph);
+        });
     }
 }
